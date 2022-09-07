1fc584561f6c7f7bb1406c93dc17a48482432927
Rafael Ferreira <Raffaferreira@users.noreply.github.com>
2022-09-06 21:20:04 -0300

    adding map configure

--- concepts/map/map.go
+++ concepts/map/map.go
@@ -3,5 +3,31 @@ package mapitem
 import "fmt"
 
 func Main() {
-	fmt.Println("")
+	var colorMap map[string]string
+	fmt.Println(colorMap)
+
+	var colorMap2 = map[string]string{"white": "#FFFFFF", "black": "#000000", "red": "#FF0000", "blue": "#0000FF", "green": "#00FF00"}
+	fmt.Println(colorMap2)
+
+	var colorMap3 = make(map[string]string)
+	colorMap3["white"] = "#FFFFFF"
+	colorMap3["black"] = "#000000"
+	colorMap3["red"] = "#FF0000"
+	colorMap3["blue"] = "#0000FF"
+	colorMap3["green"] = "#00FF00"
+	fmt.Println(colorMap3)
+	fmt.Println(colorMap["white"])
+
+	for key, value := range colorMap3 {
+		fmt.Println("Hex value of ", key, "is", value)
+	}
+
+	//update value from map
+	colorMap3["white"] = "#F0600D"
+	fmt.Println(colorMap3["white"])
+
+	//delete a key:value pair
+	delete(colorMap3, "white")
+	fmt.Print(colorMap3)
+
 }
