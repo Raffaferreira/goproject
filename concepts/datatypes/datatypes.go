0aef9fcd4c025fa67a7a7f6bd0b1a53b14ff4e9b
Rafael Ferreira <Raffaferreira@users.noreply.github.com>
2022-09-22 23:29:55 -0300

    adding data types to project

---
+++ concepts/datatypes/datatypes.go
@@ -0,0 +1,17 @@
+package datatypes
+
+import "fmt"
+
+func main() {
+
+}
+
+func integers() {
+	fmt.Println(-459)
+
+	var absoluteZero int = -459
+	fmt.Println(absoluteZero)
+
+	sum := 116 - 68
+	fmt.Println(sum)
+}
