9a69df0df9ef8335aad713b76f6069d8c289486a
Rafael Ferreira <Raffaferreira@users.noreply.github.com>
2022-11-29 17:46:41 -0300

    adding range keyword

--- concepts/range/range.go
+++ concepts/range/range.go
@@ -1,5 +1,19 @@
 package rangepackage
 
-func Main() {
+import "fmt"
 
+func Main() {
+	var evens = [5]int{2, 4, 6, 8, 10}
+
+	// using index returned from range
+	for index := range evens {
+		fmt.Printf("evens[%d] = %d \n", index, evens[index])
+	}
+
+	var str = "Golang"
+
+	// using index returned from range
+	for index := range str {
+		fmt.Printf("str[%d] = %d \n", index, str[index])
+	}
 }
